a3847262b4ad27bbed74f34d7eec7b66b2e1d603
kurpav <kurochka.p@gmail.com>
2025-04-17 15:41:19 +0300

    feat: example minor updates

--- examples/react/index.tsx
+++ examples/react/index.tsx
@@ -17,7 +17,7 @@ const App = () => {
               backgroundColor: '#f2eaf6',
             }}
           >
-            Beta
+            BETA
           </span>
         </span>
       }
@@ -46,13 +46,13 @@ const App = () => {
         fontFamily: 'Arial',
       }}
       allowFullScreen={true}
-      humanSupport={`<a href="mailto:hello@getdynamiq.ai">Talk to a human instead <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a>`}
+      // humanSupport={`<a href="mailto:hello@getdynamiq.ai">Talk to a human instead <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><line x1="5" y1="12" x2="19" y2="12"></line><polyline points="12 5 19 12 12 19"></polyline></svg></a>`}
       footerText={
         'AI Assistant is beta stage and answer might not be accurate.<br> <a href="https://getdynamiq.ai" target="_blank">Terms & Conditions</a>'
       }
-      poweredBy={
-        'Powered by <a href="https://getdynamiq.ai" target="_blank">Dynamiq</a>'
-      }
+      // poweredBy={
+      //   'Powered by <a href="https://getdynamiq.ai" target="_blank">Dynamiq</a>'
+      // }
     />
   );
 };
