a185f13c021bed668e13761c9d57ca4500d18d86
kurpav <kurochka.p@gmail.com>
2025-05-16 18:16:53 +0300

    ci: update release rules

--- release.config.mjs
+++ release.config.mjs
@@ -1,6 +1,24 @@
+import { release } from 'process';
+
 /**
  * @type {import('semantic-release').GlobalConfig}
  */
 export default {
   branches: ['main', { name: 'next', prerelease: true }],
+  plugins: [
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        preset: 'angular',
+        releaseRules: [
+          { type: 'docs', scope: 'README', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+        ],
+      },
+    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm',
+    '@semantic-release/github',
+  ],
 };
